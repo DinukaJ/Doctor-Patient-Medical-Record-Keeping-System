4f5697b7a6e90e4040890ae68f8aafa32f80efdb
pasindu07 <dmpasindu07@gmail.com>
2020-10-03 20:18:56 +0530

    intial commit

---
+++ Patient/docStatus.php
@@ -0,0 +1,116 @@
+<?php
+//Do the edits
+session_start();
+include_once(dirname( dirname(__FILE__) ).'/classes/users.php');
+include_once(dirname( dirname(__FILE__) ).'/classes/patient.php');
+include_once(dirname( dirname(__FILE__) ).'/classes/prescription.php');
+include_once(dirname( dirname(__FILE__) ).'/parts/patientSideNav.php');
+
+if(isset($_SESSION["user"]))
+{
+    $patient  = new patient();
+    $pres=new prescription();
+}
+else
+{
+    //header("Location: ../login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Header Includes -->
+    <?php include_once(dirname( dirname(__FILE__) ).'/parts/headerIncludes.php');?>
+
+    <title>Doctor Status</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Getting Side Nav -->
+            <?php getSideNav("docStatus")?>
+
+            <div class="c-12 c-l-10" style="padding-left:0px; padding-right:0px;">
+                <!-- <div class="upperPart2"> -->
+                    <div class="row" style="background-color: #c9e8e2;">
+                        <div class="c-12 c-l-3">
+                             <div class="box ">
+                                 <input class="input-field " placeholder="Enter Doctor Name">
+                             </div>
+                        </div>
+                        <div class="c-12 c-l-3">
+                            <div class="box ">
+                            <input type="date" >
+                            </div>
+                        </div>
+                        <div class="c-12 c-l-6">
+                            <div class="box">
+                            <input class="input-field " placeholder="Enter Speciality">
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+                <?php
+                $res=$patient->getPatients();
+                $i=1;
+                while($row=mysqli_fetch_assoc($res))
+                {
+                    echo '
+                    <div class="row patientDataRow">
+                        <div class="c-11">
+                            '.$i.'. '.$row['fname'].' '.$row['lname'].'
+                        </div>
+                    </div>
+                    ';
+                    $i++;
+                }
+                ?>           
+            </div>
+        </div>
+    </div>
+    <!-- The Modal -->
+    <!-- <div id="myModal" class="modal">
+
+        <div class="modal-content container">
+        <span class="close">&times;</span>
+           
+        </div>
+
+    </div> -->
+
+
+    <!-- Footer Includes -->
+    <?php include_once(dirname( dirname(__FILE__) ).'/parts/footerIncludes.php');?>
+
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks the button, open the modal 
+        //btn.onclick = 
+        function open() {
+            modal.style.display = "block";
+        }
+        if(modal)
+        {   
+            open();
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+            modal.style.display = "none";
+            }
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
+    </script>
+</body>
+</html>
