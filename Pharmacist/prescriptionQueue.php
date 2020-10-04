b684c1e2dd8bd06d0e55e71e23f8f0c91fb20d95
pasindu07 <dmpasindu07@gmail.com>
2020-10-04 19:15:18 +0530

    intial commit

---
+++ Pharmacist/prescriptionQueue.php
@@ -0,0 +1,134 @@
+<?php
+//Do the edits
+session_start();
+include_once(dirname( dirname(__FILE__) ).'/classes/users.php');
+include_once(dirname( dirname(__FILE__) ).'/classes/patient.php');
+include_once(dirname( dirname(__FILE__) ).'/classes/prescription.php');
+include_once(dirname( dirname(__FILE__) ).'/parts/patientSideNav.php');
+
+if(isset($_SESSION["user"]))
+{
+    $patient = new patient();
+    $pres=new prescription();
+}
+else
+{
+    //header("Location: ../login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Header Includes -->
+    <?php include_once(dirname( dirname(__FILE__) ).'/parts/headerIncludes.php');?>
+
+    <title>Prescriptions</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Getting Side Nav -->
+            <?php getSideNav("prescribe")?>
+
+            <div class="c-12 c-l-10" style="padding-left:0px; padding-right:0px;">
+                <div class="upperPart3">
+                    <div class="upperFirst row">
+                        <div class="c-12 c-l-3">
+                             <div class="boxSmall">
+                                 <label>Prescription No.</label>
+                             </div>
+                        </div>
+                        <div class="c-12 c-l-3">
+                            <div class="boxSmall">
+                                 <label>No. of Items</label>
+                            </div>
+                        </div>
+                        <div class="c-12 c-l-6">
+                            <div class="boxSmall">
+                                <label>No. of Prescriptions:<?php 
+                                // $pid = $pres->getUserId();
+                                // $res = $pres->getPatientPresNum($pid);
+                                // echo"<span>$res</span>"?>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row patientDataRow">
+                    <div class="c-3">
+                        1
+                    </div>
+                    <div class="c-8">
+                        1
+                    </div>
+                    <div class="c-1">
+                        <button type="submit" class="btn btnPatientView" name="viewPatient" id="viewPatient">View</button>
+                    </div>
+                </div>
+                <?php
+                // $res=$patient->getPatients();
+                // $i=1;
+                // while($row=mysqli_fetch_assoc($res))
+                // {
+                //     echo '
+                //     <div class="row patientDataRow">
+                //         <div class="c-11">
+                //             '.$i.'. '.$row['fname'].' '.$row['lname'].'
+                //         </div>
+                //         <div class="c-1">
+                //             <button type="submit" class="btn btnPatientView" name="viewPatient" id="viewPatient">View</button>
+                //         </div>
+                //     </div>
+                //     ';
+                //     $i++;
+                // }
+                ?>           
+            </div>
+        </div>
+    </div>
+    <!-- The Modal -->
+    <!-- <div id="myModal" class="modal">
+
+        <div class="modal-content container">
+        <span class="close">&times;</span>
+           
+        </div>
+
+    </div> -->
+
+
+    <!-- Footer Includes -->
+    <?php include_once(dirname( dirname(__FILE__) ).'/parts/footerIncludes.php');?>
+
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks the button, open the modal 
+        //btn.onclick = 
+        function open() {
+            modal.style.display = "block";
+        }
+        if(modal)
+        {   
+            open();
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+            modal.style.display = "none";
+            }
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
+    </script>
+</body>
+</html>
