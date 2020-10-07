179d5bc25bf26a80c183d7109c8d3e91ebb593b7
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-10-07 12:20:04 +0530

    up

--- classes/patient.php
+++ classes/patient.php
@@ -20,10 +20,20 @@ class patient extends users
         $db=new Database();
         return $db->getData("select count(id) from patient");
     }
-    public function getPatientsList()
+    public function getPatientData($id)
     {
         $db=new Database();
-        $db->getData("select * from patient where id=");
+        return $db->getData("select * from patient where id='$id'");
     }   
+    public function getPatientsList($search)
+    {
+        $db=new Database();
+        return $db->getData("select id,fname,lname from patient where id='$search' or fname like '%$search%' or lname like '%$search%'");
+    }   
+    public function updatePatient_Allergy_IN($patID,$allergy,$IN)
+    {
+        $db=new Database();
+        return $db->insert_update_delete("update patient set allergies='$allergy', impNotes='$IN' where id='$patID'");
+    }
 }
 ?>
--- handlers/patientHandler.php
+++ handlers/patientHandler.php
@@ -7,7 +7,27 @@ function addPatient()
 }
 function searchPatient()
 {
+    $output="";
+    $patient =new patient();
     $patientSearch=$_POST["patientSearch"];
+    $searchResult=$patient->getPatientsList($patientSearch);
+    if(mysqli_num_rows($searchResult))
+    {
+        while($row=mysqli_fetch_array($searchResult))
+        {
+            $output.="<li>$row[0] - $row[1] $row[2]</li>";
+        }
+    }
+    echo $output;
+}
+function updatePatient_Allergy_IN()
+{
+    $patient = new patient();
+    $patID=$_POST["patID"];
+    $allergy=$_POST["allergy"];
+    $importantNotes=$_POST["importatNotes"];
+    $stat=$patient->updatePatient_Allergy_IN($patID,$allergy,$importantNotes);
 
+    echo json_encode(array($stat));
 }
 ?>
