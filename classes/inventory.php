171b6ba834df3fef9756fc5f82183cd53a144191
pasindu07 <dmpasindu07@gmail.com>
2020-10-09 07:17:16 +0530

    added searchMed function

--- classes/inventory.php
+++ classes/inventory.php
@@ -12,5 +12,11 @@ class prescription
         $stat  = $db->insert_update_delete("insert into medicine values (NULL,'$medName','$price','$qty','$shortCode')");
         return $stat;
     }
+
+    public function searchMed($input){
+        $db = new Database();
+        $data = $db->getData("select * from medicine where name='$input' or shortCode='$input");
+        return $data;
+    }
 }
 ?>
