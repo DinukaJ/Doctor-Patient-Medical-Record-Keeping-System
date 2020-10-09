6593cf590a06f65d0d2b6c0915192bfb62206120
pasindu07 <dmpasindu07@gmail.com>
2020-10-09 22:21:40 +0530

    addBill function added

--- classes/bill.php
+++ classes/bill.php
@@ -9,7 +9,12 @@ class bill
         
     }
 
-    public function
+    //adding bill details
+    public function addBill($presId,$amt){
+        $db = new Database();
+        $stat = $db->insert_update_delete("insert into bill values ('$presId',NULL,'CURDATE()',$amt)");
+        return $stat;
+    }
 
 }
 ?>
