0dde18fe65cdb06d02d3a671d0a8460e0aae85dd
pasindu07 <dmpasindu07@gmail.com>
2020-10-09 07:12:41 +0530

    added addMEd func tion

--- classes/inventory.php
+++ classes/inventory.php
@@ -7,5 +7,10 @@ class prescription
         
     }
 
+    public function addMed($medName,$qty,$price,$shortCode){
+        $db = new Database();
+        $stat  = $db->insert_update_delete("insert into medicine values (NULL,'$medName','$price','$qty','$shortCode')");
+        return $stat;
+    }
 }
 ?>
