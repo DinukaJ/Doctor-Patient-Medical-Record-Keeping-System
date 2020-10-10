00b01368df18274ab7ededa39458d2b80d9ad271
pasindu07 <dmpasindu07@gmail.com>
2020-10-10 10:22:45 +0530

    added searchMed func

---
+++ handlers/inventoryHandler.php
@@ -0,0 +1,31 @@
+<?php
+include_once(dirname(dirname(__FILE__)).'/classes/inventory.php');
+if(isset($_POST["type"])){
+
+    if($_POST["type"]=="searchMed")
+        searchMedic();
+    if($_POST["type"]=="medData")
+        getMedDat();
+}
+
+
+function searchMedic()
+{
+    $output="";
+    $inventory = new inventory();
+    $medicSearch=$_POST["medSearch"];
+    $searchResult=$inventory->getMedList($medicSearch);
+    $count=1;
+    if(mysqli_num_rows($searchResult))
+    {
+        while($row=mysqli_fetch_array($searchResult))
+        {
+            $output.="<div class='row c-12  searchr se$count'>$row[0] - $row[1]</div>";
+            $count=$count+1;
+        }
+        $count=$count-1;
+        $output.=" <input type='hidden' id='secount' value='$count'>";
+    }
+    echo $output;
+}
+?>
