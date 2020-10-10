f6ee119609fbc95083b6e234c7469478a333738b
pasindu07 <dmpasindu07@gmail.com>
2020-10-10 17:15:37 +0530

    added med info retreiving functions

--- js/searchMed.js
+++ js/searchMed.js
@@ -2,30 +2,41 @@ var ser=document.getElementById("medId");
 var popInfo = document.getElementsByName("viewMed");
 var count=document.getElementById("secount");
 
-//keydown??
-		var a;
-		setTimeout(function(){
-			a=ser.value;
-            $.ajax({
-                url:"../handlers/inventoryHandler.php",
-                method:"POST",
-                data:{medSearch:a, type:'searchMed'},
-                success:function(data){
-                    $('#medInfo').html(data);
-                }   
-            });
-		},100)
+
+setTimeout(function(){
+    $.ajax({
+        url:"../handlers/inventoryHandler.php",
+        method:"POST",
+        data:{type:'getMed'},
+        success:function(data){
+            $('#medInfo').html(data);
+        }   
+    });
+},100);
+
+var a;
+ser.addEventListener("keydown",function(){
+		a=ser.value;
+        $.ajax({
+            url:"../handlers/inventoryHandler.php",
+            method:"POST",
+            data:{medSearch:a, type:'searchMed'},
+            success:function(data){
+                $('#medInfo').html(data);
+            }   
+        });
+});
 
 
 //handling getting med data 
 function putInventoryData(val)
 {
-    var mId=val.split(" ");
-    mId=inId[0];
+    var mId=val.split("-");
+    meId=mId[1];
     $.ajax({
         url:"../handlers/inventoryHandler.php",
         method:"POST",
-        data:{medId:mId, type:'medData'}, 
+        data:{medId:meId, type:'medData'}, 
         dataType:'json',
         success:function(data){
             // var nameVal=$('#patName').html();
