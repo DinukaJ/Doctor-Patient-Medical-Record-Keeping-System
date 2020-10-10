78f3179010edcd54e3deb81dc6193b74dd32d490
pasindu07 <dmpasindu07@gmail.com>
2020-10-10 16:05:10 +0530

    removed count variable

--- handlers/inventoryHandler.php
+++ handlers/inventoryHandler.php
@@ -15,15 +15,12 @@ function searchMedic()
     $inventory = new inventory();
     $medicSearch=$_POST["medSearch"];
     $searchResult=$inventory->getMedList($medicSearch);
-    $count=1;
     if(mysqli_num_rows($searchResult))
     {
         while($row=mysqli_fetch_array($searchResult))
         {
             $output.="<div class='row c-12  searchr se$count'>$row[0] - $row[1]</div>";
-            $count=$count+1;
         }
-        $count=$count-1;
         $output.=" <input type='hidden' id='secount' value='$count'>";
     }
     echo $output;
