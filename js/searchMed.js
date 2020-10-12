5dc0f7ff10ceee7ebc115541a9dcb8c01ef893df
pasindu07 <dmpasindu07@gmail.com>
2020-10-12 16:25:44 +0530

    minor

--- js/searchMed.js
+++ js/searchMed.js
@@ -72,24 +72,8 @@ function putInventoryData(id)
                 $('#medUpSc').val(data['shortCode']);
             })
 
-            $(".close").click(()=>{
-                close(modalViewMed);
-            });
         }
         
     });
 }
 
-//Adding data to the database
-$('#medAddForm').on('submit',function(e){
-    e.preventDefault();
-    $.ajax({
-        url:"../handlers/inventoryHandler.php",
-        method:"POST",
-        data: $('#medAddForm').serialize()+"&type=addMed",
-        success:function(data){
-            close(modalAddMed);
-            getAllMed();
-        }
-    });
-});
