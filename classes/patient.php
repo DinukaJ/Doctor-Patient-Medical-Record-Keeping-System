28e3638cb5c5718d22b169fa8047ca32ca068331
pasindu07 <dmpasindu07@gmail.com>
2020-10-16 22:36:34 +0530

    added updatePatientNonMedInfo

--- classes/patient.php
+++ classes/patient.php
@@ -36,5 +36,10 @@ class patient extends users
         $db=new Database();
         return $db->insert_update_delete("update patient set allergies='$allergy', impNotes='$IN' where id='$patID'");
     }
+    public function updatePatientNonMedInfo($patID,$fname,$lname,$phone,$age,$address)
+    {
+        $db = new Database();
+        return $db->insert_update_delete("update patient set fname='$fname',lname='$lname',phone='$phone',age='$age',address='$address' where id='$patID");
+    }
 }
 ?>
