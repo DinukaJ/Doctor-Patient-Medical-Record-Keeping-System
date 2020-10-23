4783e8fe0d1d231973594a1c4d0e216283548660
pasindu07 <dmpasindu07@gmail.com>
2020-10-23 11:00:11 +0530

    passUpdate added

--- js/mainPatient.js
+++ js/mainPatient.js
@@ -46,3 +46,42 @@ $('#usrDetailUp').on('submit',function(e){
         }
     });
 });
+
+$('#usrPassUp').on('submit',function(e){
+    e.preventDefault();
+    var patID = $("#patientID").val();
+   
+    if($('#newPass').val().match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9\S]{6,20}$/)==null)
+    {
+        $('#errorMsg').html('Password Must Contain a Uppercase Letter, Lowercase Letter, Number, and 6-20 Characters.').css('color','red');
+        $('#errorMsg').slideDown();
+        setTimeout(function(){
+            $('#errorMsg').slideUp('slow');
+        },2000);
+        return false;
+    }
+    else if($('#newPass').val()!=$('#newPassConfirm').val())
+    {
+        $('#errorMsg').html('Passwords Do Not Match!').css('color','red');
+        $('#errorMsg').slideDown();
+        setTimeout(function(){
+            $('#errorMsg').slideUp('slow');
+        },2000);
+        return false;
+    }
+    else{
+        var newPass = $('#newPass').val();
+        $.ajax({
+            url:"../handlers/patientHandler.php",
+            method:"POST",
+            data:{nPass:newPass,pid:patID,type:'upPass'},
+            success:function(){
+                $('upMsg').html('Updated Successfully');
+                $('upMsg').slideDown();
+                setTimeout(function(){
+                    $('upMsg').slideUp();
+                },2000);
+            }
+        });
+    }
+})
