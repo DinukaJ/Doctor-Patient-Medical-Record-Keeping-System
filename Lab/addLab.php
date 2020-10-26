d897e459ec8d0bff742f7e474c1892091e87e992
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-10-26 17:08:36 +0530

    Update addLab.php

--- Lab/addLab.php
+++ Lab/addLab.php
@@ -35,15 +35,15 @@ else
                 <div class="upperPart">
                     <div class="upperFirst row">
                         <div class="c-l-2">
-                            <a href="addPatients.php"><button type="button" class="btn btnNormal btnPatient active" name="addPatient" id="addPatient"><i class="fas fa-plus"></i> ADD PATIENTS</button></a>
+                            <!-- <a href="addPatients.php"><button type="button" class="btn btnNormal btnPatient active" name="addPatient" id="addPatient"><i class="fas fa-plus"></i> ADD PATIENTS</button></a> -->
                         </div>
                         <div class="c-l-2">
-                            <a href="addedPatients.php"><button type="submit" class="btn btnNormal btnPatient" name="addPatient" id="addPatient"><i class="fas fa-search"></i> VIEW PATIENTS</button></a>
+                            <!-- <a href="addedPatients.php"><button type="submit" class="btn btnNormal btnPatient" name="addPatient" id="addPatient"><i class="fas fa-search"></i> VIEW PATIENTS</button></a> -->
                         </div>
                     </div>
                     <div class="upperSecond row">
                         <div class="c-l-4">
-                            <h1 style="margin-top:5px">Enter Patient Details</h1>
+                            <h1 style="margin-top:5px">Enter Report Details</h1>
                         </div>
                         <!-- <div class="c-l-8 totText">
                             Total Patients: 10
@@ -65,25 +65,31 @@ else
                                 <div class="c-m-6">
                                     <div class="group-fields">
                                         <label>Select Patient</label>
-                                        <input type="text" class="input-field fullWidth" name="patient" id="patient" placeholder="Enter Patient ID or Name" required>
-                                    </div>
-                                </div>
-                                <div class="c-m-6">
-                                    <div class="group-fields">
-                                        <label>Phone No</label>
-                                        <input type="text" class="input-field fullWidth" name="phone" id="phone" placeholder="Phone No" required>
-                                    </div>
-                                </div>
-                                <div class="c-m-6">
-                                    <div class="group-fields">
-                                        <label>Age</label>
-                                        <input type="number" class="input-field fullWidth" name="age" id="age" placeholder="Age" required>
+                                        <input type="text" class="input-field fullWidth" name="patient" id="patient" placeholder="Field 1" required>
                                     </div>
                                 </div>
                                 <div class="c-m-12">
                                     <div class="group-fields">
-                                        <label>Address</label>
-                                        <textarea type="number" class="input-field fullWidth" name="address" id="address" placeholder="Address" required></textarea>
+                                        <label>Field 1</label>
+                                        <input type="text" class="input-field fullWidth" name="phone" id="phone" placeholder="Field 2" required>
+                                    </div>
+                                </div>
+                                <div class="c-m-12">
+                                    <div class="group-fields">
+                                        <label>Field 2</label>
+                                        <input type="text" class="input-field fullWidth" name="age" id="age" placeholder="Field 3" required>
+                                    </div>
+                                </div>
+                                <div class="c-m-12">
+                                    <div class="group-fields">
+                                        <label>Field 3</label>
+                                        <input type="text" class="input-field fullWidth" name="phone" id="phone" placeholder="Field 4" required>
+                                    </div>
+                                </div>
+                                <div class="c-m-12">
+                                    <div class="group-fields">
+                                        <label>Field 4</label>
+                                        <input type="number" class="input-field fullWidth" name="age" id="age" placeholder="Age" required>
                                     </div>
                                 </div>
                                 <div class="c-m-6">
@@ -96,7 +102,7 @@ else
                         </form>
                     </div>
                 </div>
-                <div class="row patientDataRow presBottom">
+                <!-- <div class="row patientDataRow presBottom">
                     <div class="c-12 c-l-8"></div>
                     <div class="c-6 c-l-2">
                         <button type="button" class="btn btnNormal btnPatient" name="cancel" id="cancel"><i class="fas fa-times"></i> Cancel</button>
@@ -104,7 +110,7 @@ else
                     <div class="c-6 c-l-2">
                         <button type="button" class="btn btnNormal btnPatient" name="finish" id="finish"><i class="fas fa-check"></i> Finish</button>
                     </div>
-                </div>         
+                </div>          -->
             </div>
         </div>
     </div>
