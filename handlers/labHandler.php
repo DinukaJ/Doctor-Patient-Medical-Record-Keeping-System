32b3c4e1c9b51f86dfbe6a77f99763936b0fc386
pasindu07 <dmpasindu07@gmail.com>
2020-10-27 16:55:09 +0530

    added repSearch

--- handlers/labHandler.php
+++ handlers/labHandler.php
@@ -33,7 +33,20 @@ function repSearch(){
     $output="";
     $lab = new lab();
     $data = $lab->repSearch($_POST["id"]);
-    
+    if(mysqli_num_rows($data)){
+        while(mysqli_fetch_array($data)){
+           
+            $output.="<div class='row patientDataRow'>
+            <div class='c-3' class='patId'>$row[0]</div>
+            <div class='c-4' class='repId'>$row[1]</div>
+            <div class='c-4' class='repType'>$row[3]</div>
+            <div class='c-1'>
+                <button type='button' class='btn btnPatientView viewRep' name='viewRep' id='viewRep-$row[1]'>View</button>
+            </div>
+      </div>";
+        }
+    }
+    echo $output;
 }
 
 
